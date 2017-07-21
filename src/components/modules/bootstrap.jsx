6dfa4f37f8a0c828fd979649907b340f7f85bd37
David Johnston <david@blacksheepcode.com>
2017-07-21 23:49:11 +1200

    minor bootstrap add

--- src/components/modules/bootstrap.jsx
+++ src/components/modules/bootstrap.jsx
@@ -12,22 +12,27 @@ class BootstrapModule extends React.Component {
 
         <Panel bsStyle ="primary" header="My Nice Form">
 
-        <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
-   <Tab eventKey={1} title="Tab 1">Tab 1 content</Tab>
-   <Tab eventKey={2} title="Tab 2">Tab 2 content</Tab>
-   <Tab eventKey={3} title="Tab 3" disabled>Tab 3 content</Tab>
- </Tabs>
+          <Tabs defaultActiveKey={2} id="uncontrolled-tab-example">
+            <Tab eventKey={1} title="Tab 1">
+
+
+              <DropdownButton title="Flavour" id="bg-vertical-dropdown-1">
+                <MenuItem eventKey="1">Chocolate</MenuItem>
+                <MenuItem eventKey="2">Vanilla</MenuItem>
+                <MenuItem eventKey="3">Strawberry</MenuItem>
+              </DropdownButton>
 
 
 
+            </Tab>
+            <Tab eventKey={2} title="Tab 2">
+              <p>
+                Lorem ipsum dolor sit amet, at quaestio consetetur pro. Qui doming graecis an, mazim fierent at est. An brute ullum eos, libris fuisset duo et, et duo vide ornatus detraxit. Ex facer paulo facilisi sit, posse minimum cu vim, vel animal delenit suscipiantur ex. Ut mediocrem principes definitionem sit, ne dicant possim assueverit mel. Veri idque appetere usu an, possit equidem neglegentur in duo, vidit commodo eos no. Modus habemus conceptam his at.
+              </p>
 
+            </Tab>
+          </Tabs>
 
-
-  <DropdownButton title="Flavour" id="bg-vertical-dropdown-1">
-    <MenuItem eventKey="1">Chocolate</MenuItem>
-    <MenuItem eventKey="2">Vanilla</MenuItem>
-    <MenuItem eventKey="3">Strawberry</MenuItem>
-  </DropdownButton>
 
 
           <ButtonToolbar className ="pull-right">
